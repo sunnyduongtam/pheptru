aea6f51bc75578645324e97c35c9dd5b87873118
Sunny <sunnyduongtam@gmail.com>
2024-03-06 09:36:26 +0700

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+# Pheptru
+
+Một thư viện giúp bạn tính hiệu của 2 số bất kỳ.
+Đơn giản, dễ dùng, cực kỳ nhẹ
---
+++ pheptru.js
@@ -0,0 +1,5 @@
+/* Dependencies */
+const areObjects = require('are-objects');
+const areArrays = require('are-arrays');
+const sub = (a, b) => a - b
+exports.sub = sub
